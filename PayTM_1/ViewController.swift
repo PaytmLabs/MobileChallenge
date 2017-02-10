92b7485cfcb882833f50590cd63cd9afe62cd3ca
Konstantin Klyatskin <klyatskin@gmail.com>
2017-02-09 20:37:51 -0500

    Initial Commit

---
+++ PayTM_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PayTM_1
+//
+//  Created by Konstantin Klyatskin on 2017-02-09.
+//  Copyright © 2017 Konstantin Klyatskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
