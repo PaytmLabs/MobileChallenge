66cd1ad904839d014a714eb3da0263e5b4fe8fc9
Ronak Patel <ronakp991@gmail.com>
2017-08-26 18:03:28 -0400

    MainActivity Recycler View

--- app/src/main/java/net/ronakp/mobilechallenge/activity/MainActivity.java
+++ app/src/main/java/net/ronakp/mobilechallenge/activity/MainActivity.java
@@ -2,14 +2,25 @@ package net.ronakp.mobilechallenge.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import net.ronakp.mobilechallenge.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.currencylist)
+    RecyclerView currencyListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
     }
 }
