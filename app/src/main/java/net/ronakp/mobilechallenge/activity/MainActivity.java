e85d5497ae9b25b1991a355743efb1ed101a88e1
Ronak Patel <ronakp991@gmail.com>
2017-08-26 15:03:06 -0400

    Project Created

---
+++ app/src/main/java/net/ronakp/mobilechallenge/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package net.ronakp.mobilechallenge.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.ronakp.mobilechallenge.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
