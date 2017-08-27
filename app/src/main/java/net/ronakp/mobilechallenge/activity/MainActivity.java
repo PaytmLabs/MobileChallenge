3f83fd3d5d97a922dbed7ea4f532f47b9ed88a2c
Ronak Patel <ronakp991@gmail.com>
2017-08-27 14:46:01 -0400

    Few bug fix

--- app/src/main/java/net/ronakp/mobilechallenge/activity/MainActivity.java
+++ app/src/main/java/net/ronakp/mobilechallenge/activity/MainActivity.java
@@ -3,19 +3,25 @@ package net.ronakp.mobilechallenge.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import net.ronakp.mobilechallenge.AppApplication;
 import net.ronakp.mobilechallenge.R;
+import net.ronakp.mobilechallenge.model.FixerResponse;
 import net.ronakp.mobilechallenge.service.FixerService;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.Single;
+import retrofit2.Retrofit;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
-
-    /*@Inject
-    FixerService fixerService;*/
 
     @BindView(R.id.currencylist)
     RecyclerView currencyListView;
@@ -26,5 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        ((AppApplication)getApplication()).getAppComponent().inject(this);
     }
 }
