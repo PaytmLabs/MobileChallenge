22ea6cb82b7676a4ebe8c86d5bd1bde2b765e94a
Ronak Patel <ronakp991@gmail.com>
2017-08-27 23:13:06 -0400

    Backend Running

--- app/src/main/java/net/ronakp/mobilechallenge/activity/MainActivity.java
+++ app/src/main/java/net/ronakp/mobilechallenge/activity/MainActivity.java
@@ -3,13 +3,22 @@ package net.ronakp.mobilechallenge.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import net.ronakp.mobilechallenge.R;
+import net.ronakp.mobilechallenge.model.FixerResponse;
+import net.ronakp.mobilechallenge.module.FixerHelperModule;
+import net.ronakp.mobilechallenge.service.FixerService;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FixerService fixerService;
 
     @BindView(R.id.currencylist)
     RecyclerView currencyListView;
@@ -20,5 +29,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        fixerService = FixerHelperModule.getFixerService();
+
+        fixerService.getLatestRates().enqueue(new Callback<FixerResponse>() {
+            @Override
+            public void onResponse(Call<FixerResponse> call, Response<FixerResponse> response) {
+                Log.d("hello", "post submitted to API." + response.body().getRates().getAUD());
+            }
+
+            @Override
+            public void onFailure(Call<FixerResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/net/ronakp/mobilechallenge/module/FixerModule.java
+++ app/src/main/java/net/ronakp/mobilechallenge/module/FixerModule.java
@@ -5,13 +5,7 @@ import android.content.Context;
 import net.ronakp.mobilechallenge.R;
 import net.ronakp.mobilechallenge.service.FixerService;
 
-import javax.inject.Singleton;
-
-import dagger.Module;
-import dagger.Provides;
-import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -19,13 +13,22 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class FixerModule {
-    FixerService fixerService(OkHttpClient okHttpClient, Context context) {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String url) {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+    /*FixerService fixerService(Context context) {
         return new Retrofit.Builder()
                 .baseUrl(context.getString(R.string.url))
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .client(okHttpClient)
-                .build()
-                .create(FixerService.class);
-    }
+                .build();
+    }*/
 }
